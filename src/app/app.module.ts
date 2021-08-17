2c8ed53a18a8bfbc068c837c7645ce947f35cebf
hiddenroadss <hiddenroadss@gmail.com>
2021-08-17 22:40:47 +0300

    pwa functionality added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { HeaderModule } from 'src/app/core/components/header/header.module';
 import { LoaderModule } from './core/components/loader/loader.module';
 import { HttpErrorInterceptor } from './core/interceptors/http-error-interceptor';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +18,12 @@ import { HttpErrorInterceptor } from './core/interceptors/http-error-interceptor
     HttpClientModule,
     HeaderModule,
     LoaderModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
